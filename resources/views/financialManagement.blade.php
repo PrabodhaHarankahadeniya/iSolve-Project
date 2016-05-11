66b8e34beb8f3a9bee9b205bcfd5ffc911a4e52c
Prabodha Harankahade <prabodhanh.14@cse.mrt.ac.lk>
2016-05-11 05:25:17 -0700

    new commit

--- resources/views/financialManagement.blade.php
+++ resources/views/financialManagement.blade.php
@@ -9,4 +9,28 @@
         <br>
        <!-- <img src="src/img/banner-3.jpg"/>-->
     </section>
+
+    <section class="row new-post">
+        <form action="#" method="post">
+
+            <button type="submit" class="button">Settled Cheques</button>
+            <input type="hidden" name="_token" value="{{Session::token()}}">
+        </form>
+
+        <form action="#" method="post">
+            <button type="submit" class="button">Non-settled Cheques</button>
+            <input type="hidden" name="_token" value="{{Session::token()}}">
+        </form>
+
+        <form action="#" method="post">
+            <button type="submit" class="button">Returned Cheques</button>
+            <input type="hidden" name="_token" value="{{Session::token()}}">
+        </form>
+
+        <form action="#" method="post">
+            <button type="submit" class="button">Business Report</button>
+            <input type="hidden" name="_token" value="{{Session::token()}}">
+        </form>
+
+    </section>
 @endsection
