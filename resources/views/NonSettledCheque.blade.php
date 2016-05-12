67d9babbed28002f8e168aab2e310ec0d2c3d7fe
Prabodha Harankahade <prabodhanh.14@cse.mrt.ac.lk>
2016-05-12 07:47:30 -0700

    Financial

--- resources/views/NonSettledCheque.blade.php
+++ resources/views/NonSettledCheque.blade.php
@@ -6,6 +6,43 @@
 
         <br>
         <h1>Non-Settled Cheque Report </h1>
-        <br>
+        <br><br>
+
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th align="center">Cheque No.</th>
+                <th align="center">Bank</th>
+                <th align="center">Branch</th>
+                <th align="center">Due date</th>
+                <th align="center">Amount(Rs.)</th>
+
+
+            </tr>
+            </thead>
+            <tbody>
+
+            @foreach($cheques as $cheque)
+                @if($cheque->settledStatus==0)
+                    @if($cheque->returnStatus==0)
+                        <div>
+                            <tr>
+                                <td>{{$cheque->chequeNo}}</td>
+                                <td>{{$cheque->bank}}</td>
+                                <td>{{$cheque->branch}}</td>
+                                <td>{{$cheque->date}}</td>
+                                <td align="right">{{$cheque->amount}}</td>
+
+
+                            </tr>
+
+
+                        </div>
+                    @endif
+                @endif
+            @endforeach
+            </tbody>
+        </table>
+
     </section>
 @endsection
