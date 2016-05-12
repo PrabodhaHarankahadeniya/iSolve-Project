48410d2d49b7649c7e019008a97aadf26b21ca02
ThirasaraAriyarathna <thirasaradevanmini@gmail.com>
2016-05-12 21:06:55 +0530

    updating forms

--- app/Http/Controllers/OrderManagementController.php
+++ app/Http/Controllers/OrderManagementController.php
@@ -2,20 +2,23 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderManagementcontroller extends controller{
     
     public function getPurchasePaddyForm(){
+        return view('PurchasePaddyForm');
         
     }
     public function getPurchaseRiceForm(){
-
+        return view('PurchaseRiceForm');
     }
     public function getSellRiceForm(){
-
+        return view('SellRiceForm');
     }
     public function getSellFlourForm(){
-
+        return view('SellFlourForm');
     }
     
 }
---
+++ resources/views/PurchasePaddyForm.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Thiarasara
+ * Date: 5/12/2016
+ * Time: 9:03 PM
+ */
