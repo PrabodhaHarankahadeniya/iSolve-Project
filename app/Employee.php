d2a6a771837a59edef53d04f0ca4760676a61e36
Saarrah Isthikar <Saarrah Isthikar>
2016-05-13 09:54:10 +0530

    model class for employee

--- app/Employee.php
+++ app/Employee.php
@@ -1,4 +1,58 @@
 <?php
+
+class Employee extends Model{
+
+
+    private $name;
+    private $nicNo;
+    private $gender;
+    private $teleNo;
+    private $address;
+    private $post;
+
+    public function __construct($name, $nicNo, $gender) {
+        $this->name = $name;
+        $this->gender = $gender;
+        $this->nicNo = $nicNo;
+    }
+
+    public function setAddress($address) {
+        $this->address = $address;
+    }
+
+    public function setPost($post) {
+        $this->post = $post;
+    }
+
+    public function setTeleNo($teleNo) {
+        $this->teleNo = $teleNo;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getNICNo() {
+        return $this->nicNo;
+    }
+
+    public function getAddress() {
+        return $this->address;
+    }
+
+    public function getGender() {
+        return $this->gender;
+    }
+
+    public function getPost() {
+        return $this->post;
+    }
+
+    public function getTelNo() {
+        return $this->teleNo;
+    }
+
+}
 /**
  * Created by PhpStorm.
  * User: user1
