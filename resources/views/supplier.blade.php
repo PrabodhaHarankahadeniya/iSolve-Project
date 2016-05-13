ac1b187ad97a7f944801a6578efe5552fd8ebc98
Prabodha Harankahade <prabodhanh.14@cse.mrt.ac.lk>
2016-05-13 01:41:06 -0700

    next commit

--- resources/views/supplier.blade.php
+++ resources/views/supplier.blade.php
@@ -4,11 +4,6 @@
     <link rel="stylesheet" href="src/css/homePage.css">
     <section class="row new-post">
 
-        <br>
-
-        <br><br>
-        <section class="row new-post">
-
             <br>
             <h1 align="center">Suppliers </h1>
             <br>
@@ -37,4 +32,29 @@
             </table>
 
     </section>
+    <section>
+        <form class="form-horizontal" role="form">
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="name">Name :</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name" placeholder="Enter name">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="pwd">Telephone No :</label>
+                <div class="col-sm-10">
+                    <input type="tel" class="form-control" id="pwd" placeholder="Enter telephone No.">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-sm-offset-2 col-sm-10">
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </div>
+            </div>
+        </form>
+
+
+
+    </section>
+
 @endsection
