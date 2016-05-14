33456f5921d05ff388804a20d38be4190846d393
Saarrah Isthikar <Saarrah Isthikar>
2016-05-15 05:27:18 +0530

    adding the migration scripts to centralize and identify DB schema

---
+++ database/scripts/2016_05_12_150900_create_paddystock_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaddystockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paddystock', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->String('Type');
+            $table->bigInteger('QuantityinKg');
+            $table->rememberToken();
+        });//
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('paddystock
+        ');
+    }
+}
--- readme.md
+++ readme.md
@@ -29,3 +29,4 @@ The Laravel framework is open-sourced software licensed under the [MIT license](
 ## Project Architecture and setup
 
 ## Database structure
+DB Schema is included in database/scripts. To create the schema, remove/replace the existing migration files in database/migration with above files and run php artisan migration.
