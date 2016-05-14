5145c0077362ed5073a48d5168bb6011a1da19b3
Saarrah Isthikar <Saarrah Isthikar>
2016-05-14 10:49:52 +0530

    adding Employee Navigations

--- resources/views/EmployeeManagement.blade.php
+++ resources/views/EmployeeManagement.blade.php
@@ -10,17 +10,33 @@
     </section>
     <section class="row new-post">
         <div class="btn-group-vertical" role="group">
-        <form action="{{route('linkAddEmployee')}}" method="get">
+            <form action="{{route('linkAddEmployee')}}" method="get">
 
-            <button type="submit" class="btn btn-success btn-lg btn-block">Add/Edit Employee</button><br>
-            <input type="hidden" name="_token" value="{{Session::token()}}">
-        </form>
+                <button type="submit" class="btn btn-success btn-lg btn-block">Add/Edit Employee</button>
+                <br>
+                <input type="hidden" name="_token" value="{{Session::token()}}">
+            </form>
 
-        <form action="{{route('linkAttendance')}}" method="post">
+            <form action="{{route('linkAttendance')}}" method="post">
 
-            <button type="submit" class="btn btn-success btn-lg btn-block">Marking Attendance</button>
-            <input type="hidden" name="_token" value="{{Session::token()}}">
-        </form>
+                <button type="submit" class="btn btn-success btn-lg btn-block">Marking Attendance</button>
+                <input type="hidden" name="_token" value="{{Session::token()}}">
+                <br>
+            </form>
+
+            <form action="{{route('linkAddEmployee')}}" method="get">
+
+                <button type="submit" class="btn btn-success btn-lg btn-block">Calculate EPF/ETF</button>
+                <br>
+                <input type="hidden" name="_token" value="{{Session::token()}}">
+            </form>
+
+            <form action="{{route('linkAddEmployee')}}" method="get">
+
+                <button type="submit" class="btn btn-success btn-lg btn-block">Calculate Salary</button>
+                <br>
+                <input type="hidden" name="_token" value="{{Session::token()}}">
+            </form>
         </div>
     </section>
 
