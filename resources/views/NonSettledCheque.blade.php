39bc2836a5193597f355a6a042019b99077d4ce5
Prabodha Harankahade <prabodhanh.14@cse.mrt.ac.lk>
2016-05-13 19:53:01 -0700

    updating

--- resources/views/NonSettledCheque.blade.php
+++ resources/views/NonSettledCheque.blade.php
@@ -1,7 +1,18 @@
 @extends('Layouts.master')
 
+@section('style')
+<style>
+    h1{
+
+        text-align: center;
+        font-family: Times;
+    }
+
+</style>
+
+@endsection
 @section('content')
-    <link rel="stylesheet" href="src/css/homePage.css">
+
     <section class="row new-post">
         <?php $flag=true;?>
     @if($cheques==NULL)
--- resources/views/ReturnedCheque.blade.php
+++ resources/views/ReturnedCheque.blade.php
@@ -1,7 +1,15 @@
 @extends('Layouts.master')
+@section('style')
+    <style>
+        h1{
 
+            text-align: center;
+            font-family: Times;
+        }
+
+    </style>
 @section('content')
-    <link rel="stylesheet" href="src/css/homePage.css">
+
     <section class="row new-post">
         <?php $flag=false;?>
         @if($cheques==NULL)
