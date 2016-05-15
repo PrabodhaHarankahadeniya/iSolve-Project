566c4fa6ba9cc508271d653e4d2a75de41913c3c
Saarrah Isthikar <Saarrah Isthikar>
2016-05-15 06:07:18 +0530

    insert master data related to category table

---
+++ database/scripts/feed_master_data.sql
@@ -0,0 +1,6 @@
+--Populate category table
+INSERT INTO `shakuni_rice_mills`.`category` (`cat_id`, `created_at`, `updated_at`, `gender`, `day_salary`, `ot_hourly_salary`, `epf_percentage`, `etf_percentage`, `remember_token`)
+VALUES ('1', '2016-05-10 00:00:00', NULL, 'Male', '800', '150', '8', '15', NULL);
+
+INSERT INTO `shakuni_rice_mills`.`category` (`cat_id`, `created_at`, `updated_at`, `gender`, `day_salary`, `ot_hourly_salary`, `epf_percentage`, `etf_percentage`, `remember_token`)
+VALUES ('2', '2016-05-10 00:00:00', NULL, 'Female', '650', '100', '8', '15', NULL);
