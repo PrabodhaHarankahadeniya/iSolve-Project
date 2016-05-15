ed7f434e3635afefe10d23150ba843cd31ec31a8
Saarrah Isthikar <Saarrah Isthikar>
2016-05-15 16:13:37 +0530

    make bootstrap css to be operate in offline

--- resources/views/Layouts/master.blade.php
+++ resources/views/Layouts/master.blade.php
@@ -3,8 +3,9 @@
 <head>
     <title>Shakuni Rice Mills</title>
     <script type="text/javascript" src="{{URL::to('src/js/lib/jquery-2.2.3.min.js')}}"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{URL::to('src/js/lib/jquery-2.2.3.min.js')}}">
+    <link rel="stylesheet" href="{{URL::to('src/css/lib/bootstrap.min.css')}}">
+
     @yield('style')
 
 </head>
