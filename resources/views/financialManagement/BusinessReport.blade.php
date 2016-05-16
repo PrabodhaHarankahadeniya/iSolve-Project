5fda78d7752d33e4b14178074b3f34c6de7945db
Prabodha Harankahade <prabodhanh.14@cse.mrt.ac.lk>
2016-05-16 11:16:56 -0700

    updating

--- resources/views/financialManagement/BusinessReport.blade.php
+++ resources/views/financialManagement/BusinessReport.blade.php
@@ -42,22 +42,206 @@
                 </div>
             </div>
         </form>
-    @if($purchases!=null and $cheques!=null)
-        <?php $totalCheque=0; ?>
-        @foreach($cheques as $cheque)
+    @if($purchases!=null and $payableCheques!=null and $recievableCheques!=null and $salaryAmount!=null)
+        <?php $totalPayableCheque=0; ?>
+        @foreach($payableCheques as $cheque)
             <?php
-                $totalCheque+=$cheque->amount;
+                $totalPayableCheque+=$cheque->amount;
             ?>
         @endforeach
 
+        <?php $totalRecievableCheque=0; ?>
+        @foreach($recievableCheques as $cheque)
+            <?php
+            $totalRecievableCheque+=$cheque->amount;
+            ?>
+        @endforeach
 
-        <?php $totalPurchases=0; ?>
+        <?php $totalPurchase=0; ?>
         @foreach($purchases as $purchase)
                 <?php
-                $totalPurchases+=$purchase->cash_amount;
+                $totalPurchase+=$purchase->cash_amount;
                 ?>
         @endforeach
 
+        <?php $totalOrder=0; ?>
+        @foreach($orders as $order)
+            <?php
+            $totalOrder+=$order->cash_amount;
+            ?>
+        @endforeach
+
+        <?php
+            $totalIncome=$totalRecievableCheque+$totalOrder;
+            $totalExpenditure=$totalCheque+$totalPurchase+$salaryAmount[0];
+
+
+        ?>
+        <h3>Expenditures</h3>
+        <table>
+            <tbody>
+            <tr>
+            <td colspan="4">Purchases</td>
+            <tr>
+
+            <tr>
+                <td>Purchase id</td>
+                <td>Purchase date</td>
+                <td>Purchase item</td>
+                <td>Cash amount</td>
+
+
+            </tr>
+            @foreach($purchases as $purchase)
+                <tr>
+                    <td>{{$purchase->id}}</td>
+                    <td>{{$purchase->date}}</td>
+                    <td>{{$purchase->purchase_item}}</td>
+                    <td class="amount">{{$purchase->cash_amount}}</td>
+
+
+                </tr>
+
+
+            @endforeach
+            <tr>
+                <td colspan="3">Total Amount</td>
+                <td class="amount">{{$totalPurchase}}</td>
+
+            </tr>
+
+
+            <tr><td colspan="4"></td></tr>
+            <tr>
+                <td colspan="4">Payable Cheques</td>
+            <tr>
+
+            <tr>
+                <td>Cheque No</td>
+                <td>Bank</td>
+                <td>Branch</td>
+                <td>Amount</td>
+
+
+            </tr>
+            @foreach($payableCheques as $cheque)
+                <tr>
+                    <td>{{$cheque->cheque_no}}</td>
+                    <td>{{$cheque->bank}}</td>
+                    <td>{{$cheque->branch}}</td>
+                    <td class="amount">{{$cheque->amount}}</td>
+
+
+                </tr>
+
+
+            @endforeach
+            <tr>
+                <td colspan="3">Total Amount</td>
+                <td class="amount">{{$totalPayableCheque}}</td>
+
+            </tr>
+
+            <tr><td colspan="4"> </td></tr>
+            <tr><td colspan="4"> Salories of Employees</td></tr>
+            <tr>
+                <td colspan="3">Amount</td>
+                <td class="amount">{{$salaryAmount[0]}}</td>
+
+            </tr>
+            <tr>
+                <td colspan="3">Total amount of expenditure</td>
+                <td class="amount">{{$totalExpenditure}}</td>
+
+            </tr>
+            </tbody>
+
+
+
+
+
+
+        </table>
+            <h3>Income</h3>
+            <table>
+                <tbody>
+                <tr>
+                    <td colspan="4">Orders</td>
+                <tr>
+
+                <tr>
+                    <td>Order id</td>
+                    <td>Order date</td>
+                    <td>Order item</td>
+                    <td>Cash amount</td>
+
+
+                </tr>
+                @foreach($orders as $order)
+                    <tr>
+                        <td>{{$order->id}}</td>
+                        <td>{{$order->date}}</td>
+                        <td>{{$order->purchase_item}}</td>
+                        <td class="amount">{{$order->cash_amount}}</td>
+
+
+                    </tr>
+
+
+                @endforeach
+                <tr>
+                    <td colspan="3">Total Amount</td>
+                    <td class="amount">{{$totalOrder}}</td>
+
+                </tr>
+
+
+                <tr><td colspan="4"></td></tr>
+                <tr>
+                    <td colspan="4">Recievable Cheques</td>
+                <tr>
+
+                <tr>
+                    <td>Cheque No</td>
+                    <td>Bank</td>
+                    <td>Branch</td>
+                    <td>Amount</td>
+
+
+                </tr>
+                @foreach($recievableCheques as $cheque)
+                    <tr>
+                        <td>{{$cheque->cheque_no}}</td>
+                        <td>{{$cheque->bank}}</td>
+                        <td>{{$cheque->branch}}</td>
+                        <td class="amount">{{$cheque->amount}}</td>
+
+
+                    </tr>
+
+
+                @endforeach
+                <tr>
+                    <td colspan="3">Total Amount</td>
+                    <td class="amount">{{$totalRecievableCheque}}</td>
+
+                </tr>
+
+                <tr><td colspan="4"> </td></tr>
+
+                <tr>
+                    <td colspan="3">Total amount of income</td>
+                    <td class="amount">{{$totalIncome}}</td>
+
+                </tr>
+                </tbody>
+
+
+
+
+
+
+            </table>
 
 
 
