848ab728a40385f8d1a6041d7d8e9970f3282669
suranihiranyada <suranihiranyada@gmail.com>
2016-05-17 23:08:01 +0530

    stocks updating

---
+++ resources/views/stockmanagement/StockExchange.blade.php
@@ -0,0 +1,22 @@
+@extends('Layouts.master')
+
+@section('content')
+    <link rel="stylesheet" href="src/css/homePage.css">
+    <section class="row new-post">
+
+        <br>
+        <h1>Update Stocks</h1>
+        <br><br>
+
+        <div class="row new-post">
+
+            <div class="btn-group-vertical" role="group">
+                <a class="btn btn-success btn-lg btn-block" href="{{route('PaddyRiceStockExchange')}}" role="button">Paddy Rice Stock Exchange </a><br>
+                <a href="{{route('RiceFlourStockExchange')}}" class="btn btn-success btn-lg btn-block" role="button">Rice Flour Stock Exchange</a><br>
+            </div>
+
+
+        </div>
+
+    </section>
+@endsection
