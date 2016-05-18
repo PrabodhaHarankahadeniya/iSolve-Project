38501ebf4f536eb0445206fec28a0d2e8127fbc9
Prabodha Harankahade <prabodhanh.14@cse.mrt.ac.lk>
2016-05-17 22:43:36 -0700

    updating

--- resources/views/stakeHolders/StakeHolders.blade.php
+++ resources/views/stakeHolders/StakeHolders.blade.php
@@ -7,7 +7,7 @@
         <br>
         <h1>Stake Holders</h1>
         <br>
-        <div style="float:left; width:30%;">
+        <div style="float:left; width:35%;">
             <br><br><br><br><br><br>
     <div class="btn-group-vertical" role="group">
     <a href="{{route('Customer')}}" class="btn btn-success btn-lg btn-block" role="button">Add Customer</a><br>
@@ -15,8 +15,9 @@
     </div>
 
         </div>
-        <div style="float:right; width:70%;">
-            <img  width="60%" src="src/img/Stakeholder-Icon.png"/>
+        <div style="float:right; width:65%;">
+            <br>
+            <img  width="50%" src="src/img/stakeholder.jpg"/>
             <br>
         </div>
 
