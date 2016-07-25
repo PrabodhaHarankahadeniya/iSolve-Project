c950bf4e7d201386328475abe8d6c1179d04e11d
suranihiranyada <suranihiranyada@gmail.com>
2016-07-25 18:32:48 +0530

    updating

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome!</title>
+    <title>Welcome!</title><br>
+    <h1 align="center">SHAKUNI PVT(Ltd) </h1>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
           integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="{{URL::to('src/css/main.css')}}">
