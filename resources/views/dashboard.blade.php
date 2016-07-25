e8567db57b87ab97fd820b01e681cdd172f3a92b
suranihiranyada <suranihiranyada@gmail.com>
2016-07-25 22:55:17 +0530

    updating

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,6 +14,77 @@
         font-family:SansSerif;
     }
 </style>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+    * {box-sizing:border-box}
+    body {font-family: Verdana,sans-serif;}
+
+    /* Slideshow container */
+    .slideshow-container {
+        max-width: 1500px;
+        position: relative;
+        margin: auto;
+    }
+
+    /* Caption text */
+    .text {
+        color: #f2f2f2;
+        font-size: 15px;
+        padding: 8px 12px;
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
+        text-align: center;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+        color: #f2f2f2;
+        font-size: 12px;
+        padding: 8px 12px;
+        position: absolute;
+        top: 0;
+    }
+
+    /* The dots/bullets/indicators */
+    .dot {
+        height: 13px;
+        width: 13px;
+        margin: 0 2px;
+        background-color: #bbb;
+        border-radius: 50%;
+        display: inline-block;
+        transition: background-color 0.6s ease;
+    }
+
+    .active {
+        background-color: #717171;
+    }
+
+    /* Fading animation */
+    .fade {
+        -webkit-animation-name: fade;
+        -webkit-animation-duration: 1.5s;
+        animation-name: fade;
+        animation-duration: 3s;
+    }
+
+    @-webkit-keyframes fade {
+        from {opacity: .4}
+        to {opacity: 1}
+    }
+
+    @keyframes fade {
+        from {opacity: .4}
+        to {opacity: 1}
+    }
+
+    /* On smaller screens, decrease text size */
+    @media only screen and (max-width: 300px) {
+        .text {font-size: 11px}
+    }
+</style>
+
     @endsection
 
 
@@ -23,6 +94,56 @@
 <section class="row new-post">
     <h1>SHAKUNI PVT(Ltd) </h1>
 <br>
-    <img width="100%" src="src/img/banner-3new.jpg"/></div>
 </section>
+    <div class="slideshow-container">
+        <div class="mySlides fade">
+            <img height="500px" src="src/img/rice_paddies_villa.jpg" style="width:1150px">
+        </div>
+        <div class="mySlides fade">
+            <img  height="500px" src="src/img/banner-3neww.jpg" style="width:1150px">
+        </div>
+        <div class="mySlides fade">
+            <img  height="500px"src="src/img/IMG_5248.jpg" style="width:1150px">
+        </div>
+        <div class="mySlides fade">
+            <img  height="500px"src="src/img/IMG_5142.jpg" style="width:1150px">
+        </div>
+        <div class="mySlides fade">
+            <img  height="500px"src="src/img/IMG_5230.jpg" style="width:1150px">
+        </div>
+        <div class="mySlides fade">
+            <img  height="500px"src="src/img/IMG_5202.jpg" style="width:1150px">
+        </div>
+    </div>
+    <br>
+    <div style="text-align:center">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+    </div>
+
+    <script>
+        var slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            var i;
+            var slides = document.getElementsByClassName("mySlides");
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slideIndex++;
+            if (slideIndex> slides.length) {slideIndex = 1}
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 2000); // Change image every 2 seconds
+        }
+    </script>
 @endsection
